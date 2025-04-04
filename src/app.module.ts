bdf8e5e0347911742076b883a16e80efb5fef49e
Shahiryar Arif <shahiryararif@Shahiryars-MacBook-Air.local>
2025-04-04 08:49:24 +0500

    Creating the first entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { PropertyModule } from './property/property.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { pgConfig } from './dbConfig';
-
+  
 @Module({
-  imports: [PropertyModule, TypeOrmModule.forRoot(pgConfig)],
+  imports: [
+    TypeOrmModule.forRoot(pgConfig), 
+    PropertyModule
+  ],
   controllers: [AppController],
   providers: [AppService],
-})
+}) 
 export class AppModule {}
--- src/dbConfig.ts
+++ src/dbConfig.ts
@@ -1,36 +1,16 @@
 import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
 
+console.log("test ====> ", process.env.POSTGRES_PASSWORD);
+
 export const pgConfig: PostgresConnectionOptions = {
-  type: "postgres",
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT, 10),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  type: "postgres", 
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'nest_template',
   synchronize: true,
   logging: true,
-  entities: ["dist/**/*.entity{.ts,.js}"],
-  // Add any other options you need
-  // For example, if you want to use a different naming strategy:
-  // namingStrategy: new CustomNamingStrategy(),
-  // Or if you want to enable query logging:
-  // logging: ["query", "error"],
-  // Or if you want to use a different timezone:
-  // timezone: "Z",
-  // Or if you want to use a different charset:
-  // charset: "utf8",
-  // Or if you want to use a different connection pool size:
-  // extra: {
-  //   max: 10,
-  //   min: 1,
-  //   idleTimeoutMillis: 30000,
-  //   connectionTimeoutMillis: 2000,
-  // },
-  // Or if you want to use a different connection pool strategy:
-  // connectionPool: {
-  //   max: 10,
-  //   min: 1,
-  //   idleTimeoutMillis: 30000,
-  //   connectionTimeoutMillis: 2000,
-  // },
+  entities: [__dirname + "/**/*.entity{.ts,.js}"],
+  name: "default", 
 }
