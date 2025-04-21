aa7a7cd6c872417b8d65e97d5d726bb13b68f7e2
Shahiryar Arif <shahiryararif@Shahiryars-MacBook-Air.local>
2025-04-22 04:17:37 +0500

    Configuring the JWT Module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,9 +5,16 @@ import { UserService } from 'src/user/user.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]), 
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, UserService, LocalStrategy],
 })
